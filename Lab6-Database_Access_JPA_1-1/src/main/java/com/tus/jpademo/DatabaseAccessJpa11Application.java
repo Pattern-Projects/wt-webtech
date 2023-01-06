5b157f8cce62d6b83951735372204fddc2ae43d1
John O'Sullivan <A00303153@student.ait.ie>
2023-01-06 15:31:31 +0000

    Adding 'Database Access with Spring JPA' Lab 6

---
+++ Lab6-Database_Access_JPA_1-1/src/main/java/com/tus/jpademo/DatabaseAccessJpa11Application.java
@@ -0,0 +1,15 @@
+package com.tus.jpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseAccessJpa11Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(DatabaseAccessJpa11Application.class, args);
+		System.out.println("DatabaseAccessJpa11Application Running...");
+	}
+
+}
