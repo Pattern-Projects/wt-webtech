57e0cf9bb3b7e7f098e0eb58c9cb9c3b533f2234
John O'Sullivan <A00303153@student.ait.ie>
2023-01-07 12:35:50 +0000

    Lab6: Post new user

--- Lab6-Database_Access_JPA_1-1/src/main/java/com/tus/jpademo/controllers/JpaDemoController.java
+++ Lab6-Database_Access_JPA_1-1/src/main/java/com/tus/jpademo/controllers/JpaDemoController.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,4 +40,10 @@ public class JpaDemoController {
 			throw new ResourceNotFoundException("User not found: " + userId);
 		}
 	}
+	
+	 @PostMapping("/users")
+	 public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	 }
+	 
 }
