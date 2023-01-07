415d4db5c7a359b3ee627446257cf1db48052281
John O'Sullivan <A00303153@student.ait.ie>
2023-01-07 12:47:54 +0000

    Lab6: Delete User by Id

--- Lab6-Database_Access_JPA_1-1/src/main/java/com/tus/jpademo/controllers/JpaDemoController.java
+++ Lab6-Database_Access_JPA_1-1/src/main/java/com/tus/jpademo/controllers/JpaDemoController.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,11 @@ public class JpaDemoController {
 	 @PostMapping("/users")
 	 public User createUser(@RequestBody User user) {
 		return userRepository.save(user);
+	 }
+	 
+	 @DeleteMapping("/users/{id}")
+	 public void deleteUser(@PathVariable(value="id") Long userId) {
+		 userRepository.deleteById(userId);
 	 }
 	 
 }
