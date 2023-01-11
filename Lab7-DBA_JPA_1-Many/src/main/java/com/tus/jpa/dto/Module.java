fac684db8b70d0b7a0e028d075d926b12f400527
John O'Sullivan <A00303153@student.ait.ie>
2023-01-11 13:37:17 +0000

    Lab7 complete

---
+++ Lab7-DBA_JPA_1-Many/src/main/java/com/tus/jpa/dto/Module.java
@@ -0,0 +1,61 @@
+package com.tus.jpa.dto;
+
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity @Table(name="modules")
+public class Module
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+    private Long id;
+
+	@Column(name = "module_name")
+    private String moduleName;
+
+	@Column(name = "credits")
+    private int credits;
+
+    public Module()
+    {
+    }
+
+    public Module(String moduleName, int credits)
+    {
+        this.moduleName = moduleName;
+        this.credits = credits;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getModuleName()
+    {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName)
+    {
+        this.moduleName = moduleName;
+    }
+
+    public int getCredits()
+    {
+        return credits;
+    }
+
+    public void setCredits(int credits)
+    {
+        this.credits = credits;
+    }
+}
