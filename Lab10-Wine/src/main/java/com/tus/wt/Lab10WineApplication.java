dc24025b81d5d2d86cc98360af833ebcf66b067e
John O Sullivan <A00303153@student.ait.ie>
2023-01-18 14:55:53 +0000

    Lab10: Add files

---
+++ Lab10-Wine/src/main/java/com/tus/wt/Lab10WineApplication.java
@@ -0,0 +1,14 @@
+package com.tus.wt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab10WineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab10WineApplication.class, args);
+		System.out.println("Wine time!!");
+	}
+
+}
---
+++ Lab10-Wine/src/test/java/com/tus/wt/Lab10WineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.wt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10WineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
